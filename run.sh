f2ec82f3df59f996c314c4d79ac5baef713c2fdd
nroehner <nicholasroehner@gmail.com>
2025-05-05 19:36:07 -0400

    Marking run.sh and build.sh as executable.

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
+#!/usr/bin/env bash
+
+# Docker compose up partsgenie container with optional PORT_NUMBER
+# Args: [PORT_NUMBER]
+
 PORT_NUMBER=$1
 if [ -z "$PORT_NUMBER" ]
 then
     PORT_NUMBER=5000
 fi
 
-#docker run -d -p $1:5000 partsgenie
+# docker run -d -p $1:5000 partsgenie
 PORT=$PORT_NUMBER docker-compose up -d
 
 echo '''#
